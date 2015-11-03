5cf8ab187fd47b88b253dba8f48c40da92895f89
Michael Ntambazi <michael.ntambazi@gmail.com>
2015-11-03 15:55:59 +0300

    Installed git-concat, git-coffee and also concatenated all javascript files into one file

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,21 @@
  * Created by michaelntambazi on 03/11/15.
  */
 // Bring in the gulp library(All methods and properties) and assign it to the variable gulp
-var gulp = require('gulp'),
-    gutil = require('gulp-util');
+var gulp    = require('gulp'),
+    gutil   = require('gulp-util'),
+    coffee  = require('gulp-coffee'),
+    concat  = require('gulp-concat');
+
+var jsSources = [
+    'components/scripts/app.js'
+];
 
 // Creating the log task
-gulp.task('log', function() {
-    gutil.log("This app is awesome!");
+gulp.task('js', function() {
+    // Get the script file sources
+    gulp.src(jsSources)
+        // concatenate all of them into one script file
+        .pipe(concat('script.js'))
+        // Which resides in this destination 'builds/development/js'
+        .pipe(gulp.dest('builds/development/js'))
 });
